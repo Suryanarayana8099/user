e6e0bcc63c489e3563b2a1b9c1c1b6f75be52e13
Kumar Janardhanan <kumarjanardhananbtech@gmail.com>
2024-04-19 18:18:44 +0530

    user

--- api/service.go
+++ api/service.go
@@ -10,8 +10,8 @@ import (
 	"io"
 	"time"
 
-	"github.com/microservices-demo/user/db"
-	"github.com/microservices-demo/user/users"
+	"github.com/microservices-demo-user/db"
+	"github.com/microservices-demo-user/users"
 )
 
 var (
